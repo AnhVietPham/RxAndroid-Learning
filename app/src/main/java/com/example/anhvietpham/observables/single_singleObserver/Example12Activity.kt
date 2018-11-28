84f7b804e79ba4b89a14d962ef952fbf9f3c9f1d
Unknown <pham.anh.viet.vlth@gmail.com>
2018-11-28 13:33:58 +0700

    Example 12

---
+++ app/src/main/java/com/example/anhvietpham/observables/single_singleObserver/Example12Activity.kt
@@ -0,0 +1,11 @@
+package com.example.anhvietpham.observables.single_singleObserver
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Example12Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
