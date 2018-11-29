6e44b61fd089c9f33a0f28ca3eb9a8206553800a
Unknown <pham.anh.viet.vlth@gmail.com>
2018-11-29 16:56:16 +0700

    Error observable

--- app/src/main/java/com/example/anhvietpham/observables/fire_error_observable/Example16Activty.kt
+++ app/src/main/java/com/example/anhvietpham/observables/fire_error_observable/Example16Activty.kt
@@ -3,6 +3,7 @@ package com.example.anhvietpham.observables.fire_error_observable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
+
 class Example16Activty : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
