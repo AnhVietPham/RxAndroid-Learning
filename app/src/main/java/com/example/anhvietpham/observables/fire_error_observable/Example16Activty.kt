d14eb0c36089135bc319ad299e89f0eb8cb6c600
Unknown <pham.anh.viet.vlth@gmail.com>
2018-11-29 15:49:20 +0700

    Init branch fire error observable

---
+++ app/src/main/java/com/example/anhvietpham/observables/fire_error_observable/Example16Activty.kt
@@ -0,0 +1,10 @@
+package com.example.anhvietpham.observables.fire_error_observable
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Example16Activty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
