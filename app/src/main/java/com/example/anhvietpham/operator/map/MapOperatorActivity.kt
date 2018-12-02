6903729060a8687a301d36a545baff8eb638c46b
Unknown <pham.anh.viet.vlth@gmail.com>
2018-12-03 05:54:17 +0700

    MapOperator

--- app/src/main/java/com/example/anhvietpham/operator/map/MapOperatorActivity.kt
+++ app/src/main/java/com/example/anhvietpham/operator/map/MapOperatorActivity.kt
@@ -10,10 +10,42 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
+
 class MapOperatorActivity : AppCompatActivity() {
     private var disposable: Disposable? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getUsersObservable()
+            .map { user ->
+                user.email = String.format("%s@rxjava.wtf", user.email)
+                user.name = user.name.toUpperCase()
+                return@map user
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(getUserObserver())
+        
+    }
+
+    private fun getUserObserver() : Observer<User>{
+        return object : Observer<User> {
+            override fun onComplete() {
+                Log.e("MapOperatorActivity", "All users emitted!")
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                disposable = d
+            }
+
+            override fun onNext(t: User) {
+                Log.e("MapOperatorActivity", "onNext: " + t.name + ", " + t.gender)
+            }
+
+            override fun onError(e: Throwable) {
+                Log.e("MapOperatorActivity", "onError: " + e.message)
+            }
+
+        }
     }
 
     private fun getUsersObservable() : Observable<User>{
