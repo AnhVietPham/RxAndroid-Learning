6a342e7dcee91b6c1eb21582ac339bbf6abc5490
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-01-21 22:52:06 +0700

    Init app demo Rx Java networking

---
+++ app/src/main/java/com/example/anhvietpham/rxandroidnetworking/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.anhvietpham.rxandroidnetworking
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
